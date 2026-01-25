acfbf391d6928e4048ef8185892ed572c6e657b4
@m.h.talbizadeh <m.h.talbizadeh@gmail.com>
2026-01-25 10:10:14 +0330

    Build mobile-focused home screen with background hero and centered controls

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+﻿import './Home.css'
+
+export default function Home() {
+  return (
+    <main className="home">
+      <div className="home__panel">
+        <h1 className="home__title">مافیا</h1>
+        <div className="home__actions">
+          <button className="home__button" type="button">شروع بازی</button>
+          <button className="home__button home__button--secondary" type="button">تنظیمات</button>
+        </div>
+      </div>
+    </main>
+  )
+}
